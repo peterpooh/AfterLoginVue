e38a1aa0311210e9f4bd6965870b0d3d47a31e50
peterpooh <pete_show@hotmail.com>
2020-07-27 16:08:43 +0700

    Add files via upload

---
+++ dist/precache-manifest.ac1f863f718f603f02772408fddc817d.js
@@ -0,0 +1,306 @@
+self.__precacheManifest = [
+  {
+    "revision": "3039a411a904496ae2dd",
+    "url": "/css/app.8e4a640e.css"
+  },
+  {
+    "revision": "3039a411a904496ae2dd",
+    "url": "/js/app.5f1395fd.js"
+  },
+  {
+    "revision": "e2d928018cb3ba2a805c",
+    "url": "/js/chunk-2d219ff9.0aab29fd.js"
+  },
+  {
+    "revision": "92c7b9946b80894b9d57",
+    "url": "/css/chunk-vendors.e0447626.css"
+  },
+  {
+    "revision": "92c7b9946b80894b9d57",
+    "url": "/js/chunk-vendors.0563261f.js"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/fonts/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/fonts/nucleo-icons.ff208b8d.woff2"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/fonts/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "9a18e94e832d0ebae8cee78808453e79",
+    "url": "/fonts/dbsicons.9a18e94e.woff2"
+  },
+  {
+    "revision": "6f604635a6911ba56bfdcd31451595bf",
+    "url": "/fonts/dbsicons.6f604635.ttf"
+  },
+  {
+    "revision": "8810c6d8c485aaaa4f4905ea1d0b5747",
+    "url": "/fonts/dbsicons.8810c6d8.woff"
+  },
+  {
+    "revision": "beb370d8dd65fff77bd2f78254c07289",
+    "url": "/fonts/sukhumvittadmai-text.beb370d8.eot"
+  },
+  {
+    "revision": "a318d74303278fbb7c690db6b3e554b7",
+    "url": "/fonts/sukhumvittadmai-text.a318d743.woff"
+  },
+  {
+    "revision": "6c8ceb5143ac906b0e77c0addb2f90b6",
+    "url": "/fonts/sukhumvittadmai-bold.6c8ceb51.eot"
+  },
+  {
+    "revision": "51b3765a5cca86ad7358e23940866834",
+    "url": "/fonts/sukhumvittadmai-text.51b3765a.woff2"
+  },
+  {
+    "revision": "91df9e5f1b3e7972df6f85210b38b380",
+    "url": "/fonts/sukhumvittadmai-bold.91df9e5f.woff"
+  },
+  {
+    "revision": "c184cd225fef23d64e96192414569b48",
+    "url": "/fonts/sukhumvittadmai-bold.c184cd22.woff2"
+  },
+  {
+    "revision": "882513477d7288d1eb5ff9de0c3fa9c9",
+    "url": "/fonts/sukhumvittadmai-light.88251347.eot"
+  },
+  {
+    "revision": "2cae5b5e2e83d92cb846afa13c4bc109",
+    "url": "/fonts/sukhumvittadmai-light.2cae5b5e.woff2"
+  },
+  {
+    "revision": "390c6e47aa77a71c77305c2ebd53bdc4",
+    "url": "/fonts/sukhumvittadmai-semibold.390c6e47.eot"
+  },
+  {
+    "revision": "bb329fd8492eb487c8e1652e733a8fe6",
+    "url": "/fonts/sukhumvittadmai-light.bb329fd8.woff"
+  },
+  {
+    "revision": "640681a96b0da02b948b7f0d66353463",
+    "url": "/fonts/sukhumvittadmai-semibold.640681a9.woff"
+  },
+  {
+    "revision": "4b744b65eed3fd9a3b82984c6731ff92",
+    "url": "/fonts/sukhumvittadmai-medium.4b744b65.eot"
+  },
+  {
+    "revision": "799d7b3191f33764561075eb77ba310b",
+    "url": "/fonts/sukhumvittadmai-medium.799d7b31.woff"
+  },
+  {
+    "revision": "f308c1734e73899be06e1b250db53e74",
+    "url": "/fonts/sukhumvittadmai-semibold.f308c173.woff2"
+  },
+  {
+    "revision": "b14e08056f9967860b8971ed203a4c10",
+    "url": "/fonts/sukhumvittadmai-medium.b14e0805.woff2"
+  },
+  {
+    "revision": "ae42fa524981a56f3f289447cff36eb1",
+    "url": "/img/nucleo-icons.ae42fa52.svg"
+  },
+  {
+    "revision": "30c4e8b5a2a90873795647dc3d52ac4d",
+    "url": "/fonts/sukhumvittadmai-thin.30c4e8b5.eot"
+  },
+  {
+    "revision": "f342b9beaef91737e500e15d243aa312",
+    "url": "/fonts/sukhumvittadmai-thin.f342b9be.woff"
+  },
+  {
+    "revision": "d79404977c37fb9106425f62164dfbc7",
+    "url": "/fonts/sukhumvittadmai-thin.d7940497.woff2"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/fonts/OpenSans-Semibold.33f225b8.ttf"
+  },
+  {
+    "revision": "50145685042b4df07a1fd19957275b81",
+    "url": "/fonts/OpenSans-Bold.50145685.ttf"
+  },
+  {
+    "revision": "8bac22ed4fd7c8a30536be18e2984f84",
+    "url": "/fonts/OpenSans-ExtraBold.8bac22ed.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/fonts/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "2e38456bd9727fb124d67bc4a225d682",
+    "url": "/fonts/dbsicons.2e38456b.eot"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/fonts/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "9c28ff882b7da9e37e3da52744d6f124",
+    "url": "/img/dbsicons.9c28ff88.svg"
+  },
+  {
+    "revision": "37eda17f05a358c3988d265fb3474fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/.gitkeep"
+  },
+  {
+    "revision": "9dd83916f95c567197009efb6a27f29b",
+    "url": "/img/BrainBOX_Logo.png"
+  },
+  {
+    "revision": "d97d4b19a16882a0c955fad08bfe4d5c",
+    "url": "/img/BlockTradeCalculator_Logo.png"
+  },
+  {
+    "revision": "42b3ea2c0e21c80f5b558dbce21cd786",
+    "url": "/img/dbsv-logo-min.png"
+  },
+  {
+    "revision": "740817527e3e3b733fff6e866897d13f",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "9dd83916f95c567197009efb6a27f29b",
+    "url": "/img/BrainBOX_TI_Logo.png"
+  },
+  {
+    "revision": "b0c4c166a27985e3fa4a4a2d76155953",
+    "url": "/img/DBSVOnline_Logo.png"
+  },
+  {
+    "revision": "50be2564ea249e470231d51cc9ac1435",
+    "url": "/img/BrainBOX_VA_Logo.png"
+  },
+  {
+    "revision": "14da05ade7e0718194da0b296f83f94a",
+    "url": "/img/Aspen4Browser_Logo.png"
+  },
+  {
+    "revision": "b9e50e4fce3c70cd0b48fee389a023ca",
+    "url": "/img/dbsv-logo.png"
+  },
+  {
+    "revision": "56e29127e67030b11eb630f2c329d55b",
+    "url": "/img/BrainBOX_Plus_Logo.png"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/img/faces/face-0.jpg"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/img/default-avatar.png"
+  },
+  {
+    "revision": "99e58416b89637502b40ac8350eed85a",
+    "url": "/img/faces/face-1.jpg"
+  },
+  {
+    "revision": "0e953b1ea6d6e3addd7210e9c7c420a7",
+    "url": "/img/faces/face-2.jpg"
+  },
+  {
+    "revision": "0b68eb8f1cde1fc9987a9196df05b96a",
+    "url": "/img/faces/face-4.jpg"
+  },
+  {
+    "revision": "ddec2968c15a02fcc38c22f375de2926",
+    "url": "/img/eFinStockPickup_Logo.png"
+  },
+  {
+    "revision": "776b2959a6eb9ef00b1c5ef72f5558d7",
+    "url": "/img/faces/face-3.jpg"
+  },
+  {
+    "revision": "4ab22eca4053c14a34e4bdb6390deae9",
+    "url": "/img/faces/face-5.jpg"
+  },
+  {
+    "revision": "4b87e628f4ef9988718860890b2a682f",
+    "url": "/img/faces/face-6.jpg"
+  },
+  {
+    "revision": "bf1684a30a86ba1b222aab3acff16356",
+    "url": "/img/faces/face-7.jpg"
+  },
+  {
+    "revision": "43b98081492ac3bcb4a1fac6cf709403",
+    "url": "/img/faces/tim_vector.jpe"
+  },
+  {
+    "revision": "740817527e3e3b733fff6e866897d13f",
+    "url": "/img/favicon.png"
+  },
+  {
+    "revision": "c27cd3afa361825caee83430f0ce6fcd",
+    "url": "/img/DigiS_Pro_Logo.png"
+  },
+  {
+    "revision": "0a337c37f03c0462996b7b03758fa72b",
+    "url": "/img/loading-bubbles.svg"
+  },
+  {
+    "revision": "d27fbc90c2e644dfdc9765640dc713b9",
+    "url": "/img/mask.png"
+  },
+  {
+    "revision": "838e83cf8ecdb990eebc245431feaa1f",
+    "url": "/img/faces/Blink.jpg"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/img/sidebar-1.jpg"
+  },
+  {
+    "revision": "7fcad11c424399645d0cc6d6957a4e66",
+    "url": "/img/MCycle_Logo.png"
+  },
+  {
+    "revision": "cd253e23ed052deeb80b42d2ed772183",
+    "url": "/img/sidebar-3.jpg"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/img/sidebar-2.jpg"
+  },
+  {
+    "revision": "2740e63db7c191c40fe7de069cfcc571",
+    "url": "/img/sidebar-bg.jpg"
+  },
+  {
+    "revision": "897b4cf909210560a84398d36da51983",
+    "url": "/img/sidebar-4.jpg"
+  },
+  {
+    "revision": "5789a016eee5439920ed9f9d0682fc9a",
+    "url": "/img/tmp/ChartBBP.png"
+  },
+  {
+    "revision": "9ebea76ee1225f00d882b21547a7b49f",
+    "url": "/img/sidebar-5.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bc0b77444706140cca21062aa692a487",
+    "url": "/img/Streaming_Logo.png"
+  },
+  {
+    "revision": "f4c617fdc04cfb19f3212a128b416da0",
+    "url": "/img/WealthBOX_Logo.png"
+  }
+];
